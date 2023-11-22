0b050e0bad0d574f919c7a092365273ec93768df
WhitebirdStrike <144002849+WhitebirdStrike@users.noreply.github.com>
2023-11-22 15:55:49 +0100

    Tailwind
    Har fått inn routere for de forskjellige sidene jeg skal ha.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -59,26 +59,27 @@
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+
               <a
                 href="/"
-                class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                class="bg-gray-900 shadow-lg shadow-cyan-500/50 text-white rounded-md px-3 py-2 text-sm font-medium"
                 aria-current="page"
                 >Dashboard</a
               >
               <a
                 href="/testSite"
                 class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >Team</a
+                >Produktside</a
               >
               <a
-                href="#"
+                href="/minside"
                 class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >Projects</a
+                >Min side</a
               >
               <a
-                href="#"
+                href="/omoss"
                 class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >Calendar</a
+                >Om oss</a
               >
             </div>
           </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import Test from '../views/AboutView.vue';
+import MinSideView from '../views/Min side.vue';
+import OmOssView from '../views/Om oss.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/testSite',
       name: 'aboutTest',
       component: Test,
+    },
+    {
+      path: '/minside',
+      name: 'minsSide',
+      component: MinSideView,
+    },
+    {
+      path: '/omoss',
+      name: 'omOss',
+      component: OmOssView,
     },
   ],
 });
